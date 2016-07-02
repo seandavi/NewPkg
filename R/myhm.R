26d1d6d42c07525d308730de31e1d327f3166751
Sean Davis <seandavi@gmail.com>
2016-07-02 18:53:57 -0400

    First commit

---
+++ R/myhm.R
@@ -0,0 +1,14 @@
+#' Heatmap on eset
+#'
+#' Does a cool heatmap
+#'
+#' @param eset an ExpressionSet
+#' @param count The number of genes
+#' @param ... Passed to heatmap
+#'
+#' @export
+myhm = function(eset,count=100,...) {
+  sds = apply(exprs(eset),1,sd)
+  topN = order(sds,decreasing=TRUE)[1:count]
+  heatmap(exprs(eset)[topN,],...)
+}
